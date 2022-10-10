88513b6f696e79c03dc7ca77d68911f74d2a8beb
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-10-10 14:10:39 -0700

    installed styled comps

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import React from 'react';
+import { Button, Alert} from 'react-native';
+
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout} from '@ui-kitten/components';
 
 
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default () => (
 
-  );
-}
+    <ApplicationProvider style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} {...eva} theme={eva.light}>
+   
+      <Button 
+        onPress={()  => Alert.alert('simple press')}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+  />
+    </ApplicationProvider>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+);
