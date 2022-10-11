a24162e57ad20be2361da303b61b02463367f165
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-10-11 13:52:22 -0700

    added styled comps and react

---
+++ components/navmenu/navmenu.js
@@ -0,0 +1,21 @@
+import { View,Text,} from 'react-native';
+
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import styled from 'styled-components';
+
+const Cont = styled.View`
+background-color: black;
+width: 70%%;
+height:8%;
+border-radius: 30px;
+`;
+
+export default function NavMenu(){
+    
+    return(
+        <Cont>
+           
+        </Cont>
+    )
+}
