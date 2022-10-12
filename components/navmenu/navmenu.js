68df2ce6adaf2dcb2cb061b7e8d80e272ee27ad8
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-10-11 23:07:04 -0700

    added onpress to buttons

--- components/navmenu/navmenu.js
+++ components/navmenu/navmenu.js
@@ -1,8 +1,9 @@
-import { View,Text,} from 'react-native';
+import { View,Text, StyleSheet} from 'react-native';
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import styled from 'styled-components/native';
 import { Icon } from '@ui-kitten/components';
+import { colour } from './navdata';
 
 const Cont = styled.View`
 background-color: #363630;
@@ -15,14 +16,38 @@ align-items: center;
 flex-direction: row;
 `;
 
-export default function NavMenu(){
+const Divider = styled.Text`
+color: #FEFDF4;
+font-size: 20px;
+margin-right: 7%;
+margin-left: 7%;
+`
+
+export default function NavMenu({
+ activemenu = 0,
+ onHome =()=>{},
+ onTask =()=>{},
+ onDecor =()=>{},
+ onUser =()=>{},
+}){
     
     return(
         <Cont>
-            <Icon
-            name='star'
-            fill="white"
-            />
+            <Icon name='home' fill={colour[activemenu].home} style={styles.icon} onPress={onHome}/>
+            <Divider>|</Divider>
+            <Icon name='clipboard' fill={colour[activemenu].board} style={styles.icon} onPress={onTask}/>
+            <Divider>|</Divider>
+            <Icon name='brush' fill={colour[activemenu].brush} style={styles.icon} onPress={onDecor}/>
+            <Divider>|</Divider>
+            <Icon name='person' fill={colour[activemenu].user} style={styles.icon} onPress={onUser}/>
+        
         </Cont>
     )
 }
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 35,
+      height: 35,
+    },
+  });
