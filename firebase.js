eddee39a2e5f3cbd816c642b12f1c15c94c4e186
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-11-13 18:57:16 -0800

    added add user to databaase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
