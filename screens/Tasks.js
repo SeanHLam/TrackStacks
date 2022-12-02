48d4e3cccb52cf4c52df73723ae1e4e972946f45
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-12-02 01:47:36 -0800

    fixed rdit task

--- screens/Tasks.js
+++ screens/Tasks.js
@@ -4,7 +4,7 @@ import { ApplicationProvider, Calendar, IconRegistry} from '@ui-kitten/component
 import NavMenu from '../components/navmenu/navmenu.js';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { useState, useEffect} from 'react';
-import { Wrapper, NavWrapper, SliderCont, AddCont, ConfettiContainer } from '../styles/global.js';
+import { Wrapper, NavWrapper, SliderCont, AddCont } from '../styles/global.js';
 import Header from '../components/header/header.js';
 import { default as theme } from "../assets/TSTheme.json";
 import TaskList from '../components/tasklist/tasklist.js';
@@ -187,9 +187,8 @@ export default function Tasks({navigation, route}) {
               </TaskList> ) 
               
             }
-            <ConfettiContainer>
-            {confetti === true ? <ConfettiCannon count={60} origin={{x:200, y:0}} fallSpeed={3000} fadeOut={true}/> : ''}
-            </ConfettiContainer>
+
+            {/* {confetti === true ? <ConfettiCannon count={60} origin={{x:200, y:-8}} fallSpeed={3000} fadeOut={true}/> : ''} */}
             
           </Wrapper>
         </SliderCont>
